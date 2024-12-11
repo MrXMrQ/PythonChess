266e47427504fcb0380b4edb5d3402b9f731b448
Marvin Bäumer <m.baeumee.industries@gmail.com>
2024-12-11 16:19:41 +0100

    update: main
    some changes nothing special.

--- MyFigure.py
+++ MyFigure.py
@@ -1,5 +1,25 @@
 class Figure:
+    """
+    Represents a figure in a game or simulation, such as a chess piece or a game character.
+
+    Attributes:
+        name (str): The name of the figure.
+        start_field (tuple): The starting position of the figure as a tuple of coordinates (e.g., (x, y)).
+        current_field (tuple): The current position of the figure as a tuple of coordinates.
+        is_Friend (bool): A flag indicating if the figure is a friendly entity. Defaults to True.
+    """
+
     def __init__(self, name: str, start_field: tuple, current_field: tuple, is_Friend: bool = True) -> None:
+        """
+        Initializes a new instance of the Figure class.
+
+        Args:
+            name (str): The name of the figure.
+            start_field (tuple): The starting position of the figure as a tuple of coordinates.
+            current_field (tuple): The current position of the figure as a tuple of coordinates.
+            is_Friend (bool): A flag indicating if the figure is friendly. Defaults to True.
+        """
+        
         self.name = name
         self.start_field = start_field
         self.current_field = start_field
--- main.py
+++ main.py
@@ -1,29 +1,25 @@
+#Tests for figures
+from Test import Test_Figures
+
+#Chess pieces
+from MyFigure import Figure
 from MyPawn import Pawn
+from MyRook import Rook
 
-pawn_01 = Pawn("Pawn01", (1,2))
-pawn_02 = Pawn("Pawn02", (2,2))
-pawn_03 = Pawn("Pawn03", (3,2))
-pawn_04 = Pawn("Pawn04", (4,2))
-pawn_05 = Pawn("Pawn05", (5,2))
-pawn_06 = Pawn("Pawn06", (6,2))
-pawn_07 = Pawn("Pawn07", (7,2))
-pawn_08 = Pawn("Pawn08", (8,2))
+def fill_figures() -> list:
+    pawns: Pawn = []
+    for i in range(8):
+        pawns.append(Pawn("Pawn {}".format(i + 1), (i + 1,2)))
 
-pawns: Pawn = [pawn_01, pawn_02, pawn_03, pawn_04, pawn_05, pawn_06, pawn_07, pawn_08]
+    rooks: Rook = [Rook("Rook0", (1,1)), Rook("Rook1", (8,1))]
+        
+    return[pawns, rooks]
 
-print(pawns[0].current_field)
-pawns[0].move((1,3))
-print(pawns[0].current_field)
-pawns[0].move((1,4))
-print(pawns[0].current_field)
-pawns[0].move((1,5))
-print(pawns[0].current_field)
-pawns[0].move((1,6))
-print(pawns[0].current_field)
-pawns[0].move((1,7))
-print(pawns[0].current_field)
-pawns[0].move((1,8))
-print(pawns[0].current_field)
-pawns[0].move((1,9))
-pawns[0].move((1,10))
-print(pawns[0].current_field)
+def start_test():
+    print(figures)
+    testing_area: Test_Figures = Test_Figures(figures[0], figures[1])
+    testing_area.pawn_test()
+
+figures: Figure = fill_figures()
+
+start_test()
