84bc27a5cd2a81b60001beab556af2c1127b7378
Marvin Bäumer <m.baeumee.industries@gmail.com>
2025-02-18 03:00:06 +0100

    update: main.py

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 # Pieces
-from Pieces.MyPiece import Piece
+from Pieces.piece import Piece
+from Pieces.pawn import Pawn
+from Pieces.limited_range_piece import LimitedRangePiece
 
-# Chessboard
-from tkinter import *
-from GUI.Chessboard.MyChessboard import Chessboard
+from GUI.chessboard import Chessboard
 
 def fill_figures() -> list:
-    pieces: Piece = []
-    pieces.append(Piece("Rook 1", (1,1), [(1, 0), (-1, 0), (0, 1), (0, -1)], True))
-    pieces.append(Piece("Rook 2", (1,8), [(1, 0), (-1, 0), (0, 1), (0, -1)], True))
-    pieces.append(Piece("Bishop 1", (1,3), [(1, -1), (1, 1), (-1,-1), (-1,1)], True))
-    pieces.append(Piece("Bishop 1", (1,6), [(1, -1), (1, 1), (-1,-1), (-1,1)], True))
-    pieces.append(Piece("Queen", (1,4), [(1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (1, 1), (-1,-1), (-1,1)], True))
+    rooks = [Piece("♖", (1,1), [(1, 0), (-1, 0), (0, 1), (0, -1)]), Piece("♖", (1,8), [(1, 0), (-1, 0), (0, 1), (0, -1)])]
+    bishop = [Piece("♗", (1,3), [(1, -1), (1, 1), (-1,-1), (-1,1)]), Piece("♗", (1,6), [(1, -1), (1, 1), (-1,-1), (-1,1)])]
+    queen = [Piece("♕", (1,4), [(1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (1, 1), (-1,-1), (-1,1)])]
+    knight = [LimitedRangePiece("♘", (1,2), [(2, 1),(2, -1),(-2, 1),(-2, -1),(1, 2),(1, -2), (-1, 2),(-1, -2)]), LimitedRangePiece("♘", (1,7), [(2, 1),(2, -1),(-2, 1),(-2, -1),(1, 2),(1, -2), (-1, 2),(-1, -2)])]
+    king = [LimitedRangePiece("♔", (1,5), [(1, 0), (-1, 0), (0, 1), (0, -1),(1, 1), (1, -1), (-1, 1), (-1, -1)])]
 
-    pieces.append(Piece("Knight 1", (1,2), [(2, 1),(2, -1),(-2, 1),(-2, -1),(1, 2),(1, -2), (-1, 2),(-1, -2)], True, True))
-    pieces.append(Piece("Knight 1", (1,7), [(2, 1),(2, -1),(-2, 1),(-2, -1),(1, 2),(1, -2), (-1, 2),(-1, -2)], True, True))
-    
-    pieces.append(Piece("King", (1,5), [(1, 0), (-1, 0), (0, 1), (0, -1),(1, 1), (1, -1), (-1, 1), (-1, -1)], True, True))
-
-    pieces.append(Piece("Enemy", (5,5), [(0,0)], False))
-
+    pawns = []
     for i in range(8):
-        pieces.append(Piece("Pawn {}".format(i + 1), (2,i + 1), [(1, 0)], True, True, True))
+        pawns.append(Pawn("♙", (2, i+1), [(1,0)], True))
+
+    pieces: Piece = [rooks, bishop, queen, knight, king, pawns]
+    pieces.append([Piece("♟", (5,5), [(0,0)], False)])
 
     return pieces
 
@@ -29,4 +25,8 @@ pieces: Piece = fill_figures()
 chessboard = Chessboard(pieces)
 
 window = chessboard.create_window()
+
+for piece in pieces[5]:
+    piece.window = window
+
 window.mainloop()
