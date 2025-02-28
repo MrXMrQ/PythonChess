80229285ebec8bca26078213c706536fb2deaf95
Marvin Bäumer <m.baeumee.industries@gmail.com>
2025-02-28 01:05:26 +0100

    refactor: king class
    docstrings, better logic

--- Pieces/king.py
+++ Pieces/king.py
@@ -1,74 +1,87 @@
-from Pieces.limited_range_piece import LimitedRangePiece
 from Pieces.piece import Piece
 
 from typing import override
 
 def get_pawn():
-    from Pieces.pawn import Pawn  # Import erst bei Funktionsaufruf
+    """
+    Dynamically loads the Pawn class to avoid circular imports.
+    """
+
+    from Pieces.pawn import Pawn
     return Pawn
 
-class King(LimitedRangePiece):
+class King(Piece):
+    """
+    Represents the King piece in chess.
+
+    Attributes:
+        current_field (tuple): The King's current position on the board.
+        _team (str): The color of the King ("white" or "black").
+        _directions (list): A list of all possible movement directions.
+
+    Methodes:
+        compute_moves(self, chessboard) -> list:   
+            Computes all valid moves for the king piece while considering other pieces potential threats.
+
+        _get_surrounding_fields(self, chessboard: dict, enemy_king: Piece) -> list:
+            return a list from the surrounding fields of the enemy king
+    """
+    
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
     @override
     def compute_moves(self, chessboard) -> list:
-        valid_moves = []
-        directions = self.directions
-        
+        """
+        Computes all valid moves for the king piece while considering other pieces potential threats.
+
+        Args:
+            chessboard (dict): The current chessboard state, mapping positions to pieces.
+
+        Returns:
+            list: A list of valid moves that are not under threat.
+        """
+
+        valid_moves = [] 
         row, col = self.current_field
 
-        for direction in directions:
-            new_row = row + direction[0]
-            new_col = col + direction[1]
-            next_field = (new_row, new_col)
+        for drY, drX in self._directions:
+            next_field = (row + drY, col + drX)
 
             if next_field in chessboard:
-                if self._team == "white":
-                    if chessboard[next_field] is None or not chessboard[next_field]._team == "white":
-                        valid_moves.append(next_field)
-                elif self._team == "black":
-                    if chessboard[next_field] is None or not chessboard[next_field]._team == "black":
-                        valid_moves.append(next_field)
-                else:
-                    raise ValueError(f"ERROR: object have no team: {self._team}")
+                piece = chessboard[next_field]
+                if piece is None or piece.team != self._team:
+                    valid_moves.append(next_field)    
+   
+        threatened_moves = []
                 
-        other_moves = []
-                
-        for i in chessboard:
-            piece: Piece = chessboard[i]
-            if piece is not None and piece.team != self._team:
-                
+        for position, piece in chessboard.items():
+            if piece and piece.team != self._team:
                 if isinstance(piece, get_pawn()):
-                    other_moves += piece.diagonal_fields()
-                    continue
+                    threatened_moves += piece._diagonal_fields()
+                elif isinstance(piece, King):
+                    threatened_moves += self._get_surrounding_fields(chessboard, piece)   
+                else:
+                    threatened_moves += piece.compute_moves(chessboard)
 
-                if isinstance(piece, King):
-                    other_moves += self.get_sorunding_fields(chessboard, piece)
-                    continue
-                
-                other_moves += piece.compute_moves(chessboard)  
-
-        other_moves.sort()
-
-        for i in other_moves:
-            if i in valid_moves:
-                valid_moves.remove(i)  
-
-        return valid_moves
+        return [move for move in valid_moves if move not in threatened_moves]
     
-    def get_sorunding_fields(self, chessboard: dict, enemy_king: Piece) -> list:
+    def _get_surrounding_fields(self, chessboard: dict, enemy_king: Piece) -> list:
+        """
+        Returns all surrounding fields of the given enemy king that are on the chessboard.
+
+        Args:
+            chessboard (dict): The current state of the chessboard.
+            enemy_king (Piece): The enemy king whose surrounding fields are to be checked.
+
+        Returns:
+            list: A list of valid surrounding fields as tuples.
+        """
+
         x, y = enemy_king._current_field
-        fields = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x - 1, y - 1), (x + 1, y + 1), (x - 1, y + 1), (x + 1, y - 1)]
+        fields = [
+            (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), 
+            (x - 1, y - 1), (x + 1, y + 1), (x - 1, y + 1), (x + 1, y - 1)
+        ]
 
-        for field in fields:
-            if field not in chessboard:
-                fields.remove(field)
-
-        return fields
-
-    def check(self) -> None:
-        pass
-
-    def check_mate(self) -> None:
-        pass
+        return [field for field in fields if field in chessboard]
