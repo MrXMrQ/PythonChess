d2cdda0e4cc2c714f9dbc6df90fc68e79b46a672
Marvin Bäumer <m.baeumee.industries@gmail.com>
2025-02-28 01:05:05 +0100

    refactor: pawn class
    docstrings, better logic

--- Pieces/pawn.py
+++ Pieces/pawn.py
@@ -1,14 +1,41 @@
 from tkinter import Tk, Button, Toplevel, Label
 
+from Pieces.limited_range_piece import LimitedRangePiece
 from GUI.custom_banner import CustomBanner
 from Pieces.piece import Piece
-from Pieces.limited_range_piece import LimitedRangePiece
 
 from GUI.custom_button import CustomButton
 
 from typing import override
 
 class Pawn(Piece):
+    """
+    Represents a pawn piece in a chess game.
+
+    Attributes:
+        _window (Tk): The Tkinter window used for UI interactions.
+        _current_field (tuple): The current position of the pawn on the board.
+        _moved (bool): Indicates whether the pawn has moved before.
+        _team (str): The team color ("white" or "black") of the pawn.
+        _name (str): The name or symbol representing the pawn.
+
+    Methods:
+        compute_moves(chessboard: dict) -> list:
+            Computes all valid moves for the pawn based on its movement rules.
+
+        apply_move(chessboard: dict, field: tuple, banner: CustomBanner) -> dict:
+            Moves the pawn to the given field and handles promotion if applicable.
+
+        _diagonal_fields(one_step) -> list[tuple, tuple]:
+            Returns the diagonal attack positions based on the pawn's current move direction.
+
+        _open_popup(chessboard: dict, team: str, banner: CustomBanner) -> None:
+            Opens a UI popup to allow the player to select a promotion piece.
+
+        _replace(chessboard: dict, piece: Piece, banner: CustomBanner) -> None:
+            Replaces the current pawn with a promoted piece.
+    """
+
     def __init__(self, *args, **kwargs) -> None:
         self._window = None
 
@@ -16,79 +43,82 @@ class Pawn(Piece):
 
     @override
     def compute_moves(self, chessboard: dict) -> list:
+        """
+        Computes all valid moves for the pawn piece based on the current chessboard state.
+            - two steps or one step on start
+            - diagonal attack
+
+        Args:
+            chessboard (dict): The current state of the chessboard, mapping positions to pieces.
+
+        Returns:
+            list: A list of valid moves as tuples.
+
+        Raises:
+            TypeError: If chessboard is not a dictionary.
+        """
+
         valid_moves = []
         row, col = self.current_field
+        direction = 1 if self._team == "white" else - 1
 
-        if self._team == "white":
-            one_step = (row + 1, col)
-        elif self._team == "black":
-            one_step = (row - 1, col)
-        else:
-            raise ValueError(f"ERROR: object have no team: {self._team}") 
+        one_step = (row + direction, col)
 
         if one_step in chessboard and chessboard[one_step] is None:
             valid_moves.append(one_step)
 
-            if self._team == "white":
-                two_steps = (row + 2, col)
-            elif self._team == "black":
-                two_steps = (row - 2, col)
-            else:
-                raise ValueError(f"ERROR: object have no team: {self._team}")
-
+            two_steps = (row + 2 * direction, col)
             if not self._moved and two_steps in chessboard and chessboard[two_steps] is None:
                 valid_moves.append(two_steps)
 
-        diagonal_left, diagonal_right = self.diagonal_fields()
+        for diagonal in self._diagonal_fields():
+            if diagonal in chessboard and chessboard[diagonal] is not None:
+                if chessboard[diagonal]._team != self._team:
+                    valid_moves.append(diagonal)   
 
-        if diagonal_left in chessboard and chessboard[diagonal_left] is not None:
-            if self._team == "white":
-                if not chessboard[diagonal_left]._team == "white":
-                    valid_moves.append(diagonal_left)
-            elif self._team == "black":
-                if not chessboard[diagonal_left]._team == "black":
-                    valid_moves.append(diagonal_left)
-            else:
-                raise ValueError(f"ERROR: object have no team: {self._team}")
-
-        if diagonal_right in chessboard and chessboard[diagonal_right] is not None:
-            if self._team == "white":
-                if not chessboard[diagonal_right]._team == "white":
-                    valid_moves.append(diagonal_right)
-            elif self._team == "black":
-                if not chessboard[diagonal_right]._team == "black":
-                    valid_moves.append(diagonal_right)
-            else:
-                raise ValueError(f"ERROR: object have no team: {self._team}")
-        
         return valid_moves
     
-    def diagonal_fields(self) -> tuple[tuple, tuple]:
-        row, col = self._current_field     
+    def _diagonal_fields(self) -> list[tuple, tuple]:
+        field = self._current_field
+        direction = 1 if self._team == "white" else - 1
 
-        if self._team == "white":
-            diagonal_left = (row + 1, col - 1)
-            diagonal_right = (row + 1, col + 1)
-        elif self._team == "black":
-            diagonal_left = (row - 1, col - 1)
-            diagonal_right = (row - 1, col + 1)
-        else:
-            raise ValueError(f"ERROR: object have no team: {self._team}")
-        
-        return (diagonal_left, diagonal_right)
+        return [(field[0] + direction, field[1] - 1), (field[0] + direction, field[1] + 1)]
 
     @override
-    def apply_move(self, field: tuple, chessboard: dict, banner: CustomBanner) -> dict:
-        board = super().apply_move(field, chessboard, banner)
+    def apply_move(self, chessboard: dict, field: tuple, banner: CustomBanner) -> dict:
+        """
+        Moves the piece to the specified field and handles pawn promotion.
 
-        if self._current_field[0] == 8:
-            self.open_popup(chessboard, "white", banner)
-        elif self._current_field[0] == 1:
-            self.open_popup(chessboard, "black", banner)
+        Args:
+            chessboard (dict): A dictionary representing the current state of the chessboard.
+            field (tuple): The target position (row, column) to move the piece to.
+            banner (CustomBanner): The UI banner object used to update the game state.
+
+        Returns:
+            dict: The updated chessboard after the move.
+        """
+
+        board = super().apply_move(chessboard, field, banner)
+
+        if self._current_field[0] in {1, 8}:
+            team_color = "white" if self._current_field[0] == 8 else "black"
+            self._open_popup(chessboard, team_color, banner)
 
         return board
     
-    def open_popup(self, chessboard: dict, team: str, banner: CustomBanner) -> None:
+    def _open_popup(self, chessboard: dict, team: str, banner: CustomBanner) -> None:
+        """
+        Opens a popup window for pawn promotion selection.
+
+        Args:
+            chessboard (dict): A dictionary representing the current state of the chessboard.
+            team (str): The team color ("white" or "black") of the promoting piece.
+            banner (CustomBanner): The UI banner object used to update the game state.
+
+        Returns:
+            None
+        """
+
         popup = Toplevel(self._window)
         popup.geometry("400x400")
         popup.title("Promotion Selection")
@@ -98,7 +128,6 @@ class Pawn(Piece):
         popup.grab_set()    
         popup.protocol("WM_DELETE_WINDOW", lambda: None)
 
-        
         popup.grid_rowconfigure(3,weight=1)
         popup.grid_columnconfigure(2,weight=1)
 
@@ -111,17 +140,27 @@ class Pawn(Piece):
         bishop = Piece("♗" if team == "white" else "♝", self._current_field, [(1, -1), (1, 1), (-1,-1), (-1,1)], 3, team)
         queen = Piece("♕" if team == "white" else "♛", self._current_field, [(1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (1, 1), (-1,-1), (-1,1)], 9, team)
         
-        Button(popup, text="♘" if team == "white" else "♞", command=lambda: [self.replace(knight, chessboard, banner), popup.destroy()], **btn_style).pack(pady=10)
-        Button(popup, text="♖" if team == "white" else "♜", command=lambda: [self.replace(rook, chessboard, banner), popup.destroy()], **btn_style).pack(pady=10)
-        Button(popup, text="♗" if team == "white" else "♝", command=lambda: [self.replace(bishop, chessboard, banner), popup.destroy()], **btn_style).pack(pady=10)
-        Button(popup, text="♕" if team == "white" else "♛", command=lambda: [self.replace(queen, chessboard, banner), popup.destroy()], **btn_style).pack(pady=10)
+        for piece, symbol in zip([knight, rook, bishop, queen], ["♘", "♖", "♗", "♕"] if team == "white" else ["♞", "♜", "♝", "♛"]):
+            Button(popup, text=symbol, command=lambda p=piece: [self._replace(chessboard, p, banner), popup.destroy()], **btn_style).pack(pady=10)
 
         self._window.wait_window(popup) 
 
-    def replace(self, piece: Piece, chessboard: dict, banner: CustomBanner) -> None:
+    def _replace(self, chessboard: dict, piece: Piece, banner: CustomBanner) -> None:
+        """
+        Replaces the current piece with a promoted piece on the chessboard.
+
+        Args:
+            chessboard (dict): A dictionary representing the current state of the chessboard.
+            piece (Piece): The promoted piece to replace the current one.
+            banner (CustomBanner): The UI banner object used to display game updates.
+
+        Returns:
+            None
+        """
+
         chessboard[self._current_field] = piece
         CustomButton._button_registry[self.current_field]._content = piece
-        banner.change_text(self._team[0].upper() + self._team[1:] + f" PROMOTES {self._name} to {chessboard[self._current_field]._name}", None)
+        banner.change_text(f"{self._team.capitalize()} PROMOTES {self._name} to {piece.name}", None)
         
     @property
     def window(self) -> Tk:
